d33ef7bb088b960c30d4ec33308c6391db389258
Victor <vmadridh16@gmail.com>
2023-12-20 14:26:16 +0100

    Add Pool and Factory for Tiles

---
+++ Assets/3_Scripts/PoolManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class PoolManager<T>: MonoBehaviour where T : MonoBehaviour
+{
+    public T Prefab;
+    [SerializeField]
+    private int initialPoolSize = 2;
+    
+    private ObjectPool<T> objectPool;
+
+    public void Initialize(Action<T> onGet = null, Action<T> onRelease = null, Action<T> onDestroy = null)
+    {
+        objectPool = new ObjectPool<T>(OnCreate, onGet, onRelease, onDestroy, true, initialPoolSize);
+    }
+    
+    protected virtual T OnCreate()
+    {
+        T obj = Instantiate(Prefab, transform);
+        return obj;
+    }
+    
+    public virtual T Get(Vector3 position, Quaternion direction, Transform parent)
+    {
+        T spawnedObject = objectPool.Get();
+        Transform trans = spawnedObject.transform;
+        Quaternion rotation = Prefab.transform.rotation * direction;
+        trans.position = position;
+        trans.rotation = rotation;
+        trans.SetParent(parent);
+        return spawnedObject;
+    }
+
+    public virtual void Release(T target)
+    {
+        Debug.Log("RELEASE THIS", target);
+        objectPool.Release(target);
+        target.transform.SetParent(transform);
+    }
+}
---
+++ Assets/3_Scripts/TowerTileFactory.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TowerTileFactory : Singleton<TowerTileFactory>
+{
+    [SerializeField]
+    private PoolManager<TowerTile> poolTiles;
+    [SerializeField]
+    private List<PoolManager<TowerTile>> poolSpecialTiles;
+
+    private bool IsPoolingEnabled => RemoteConfig.BOOL_OBJECT_POOL_ENABLED && Application.isPlaying;
+    
+    public void Initialize()
+    {
+        if (!IsPoolingEnabled)
+        {
+            return;
+        }
+        
+        poolTiles.Initialize(OnGetTile, OnReleaseTile);
+        poolSpecialTiles.ForEach(pool => pool.Initialize(OnGetTile, OnReleaseTile));
+    }
+
+
+    public TowerTile Create(Vector3 position, Quaternion direction, Transform parent, float specialTileChance)
+    {
+        PoolManager<TowerTile> pool = GetPool(specialTileChance);
+        
+        if (IsPoolingEnabled)
+        {
+            return pool.Get(position, direction, parent);
+        }
+
+        TowerTile prefab = pool.Prefab;
+        Quaternion rotation = direction * prefab.transform.rotation;
+        TowerTile towerTile = Instantiate(prefab, position, rotation, parent);
+        OnGetTile(towerTile);
+        return towerTile;
+    }
+
+    private PoolManager<TowerTile> GetPool(float specialTileChance)
+    {
+        bool isSpecial = Random.value <= specialTileChance;
+        
+        return !isSpecial ? poolTiles : poolSpecialTiles[Random.Range(0, poolSpecialTiles.Count)];
+    }
+
+    public void Delete(TowerTile tile)
+    {
+        Debug.Log("DELETE", tile);
+        if (IsPoolingEnabled)
+        {
+            poolTiles.Release(tile);
+            return;
+        }
+
+        OnReleaseTile(tile);
+        if (Application.isPlaying)
+        {
+            Destroy(tile.gameObject);
+        }
+        else
+        {
+            DestroyImmediate(tile.gameObject);
+        }
+    }
+
+    private void OnGetTile(TowerTile tile)
+    {
+        tile.Get();
+    }
+    
+    private void OnReleaseTile(TowerTile tile)
+    {
+        tile.Release();
+    }
+}
