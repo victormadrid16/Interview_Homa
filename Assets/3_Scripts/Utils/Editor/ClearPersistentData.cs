7cb1044a3e6af99960b38a3cb42ee5289d0ea9de
Victor <vmadridh16@gmail.com>
2023-12-21 15:04:10 +0100

    Add clear data tool

---
+++ Assets/3_Scripts/Utils/Editor/ClearPersistentData.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ClearPersistentData 
+{
+    [MenuItem("Tools/ClearAllData")]
+    private static void ClearAllData()
+    {
+        PlayerPrefs.DeleteAll();
+        string path = Application.persistentDataPath;
+
+        try
+        {
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string file in files)
+            {
+                File.Delete(file);
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Failed to clear persistent data: {e.Message}");
+        }
+    }
+}
